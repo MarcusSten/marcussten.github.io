1d011dc8a9f40cce7983c5ee68c52a42becd8654
MarcusSten <malmeloon@gmail.com>
2021-04-16 12:14:11 +0300

    edit coursJS1/lesson4

--- CourseJS1/lesson4/js/1.js
+++ CourseJS1/lesson4/js/1.js
@@ -13,11 +13,14 @@ function myScript1(){
             numbers.tens = num.charAt(1);
             numbers.figure = num.charAt(2);
             console.log("В заданном числе содержится: " + numbers.figure + " единиц, " + numbers.tens + " десятков, " + numbers.hundred + " сотен.");  
-        } else{
-            console.log("Вы ввели значение больше заданного или не число " + numbers);
+        } else if(num >= 1000){
+            console.log("Вы ввели значение больше заданного. Свойств в объекте: " + Object.keys(numbers).length);
+        } else {
+            console.log("Вы ввели не число ");
         }
     }
 
     score();
+} 
+    
 
-}
--- CourseJS1/lesson5/js/1.js
+++ CourseJS1/lesson5/js/1.js
@@ -1,64 +1,22 @@
 function myScript1(){
 
-    var product = [
-        {
-            id: 0001,
-            title: "Jacket",
-            description: "Amazing red jacket",
-            price: 250,
-            count: 1,
-            color: "Red",
-            size: "S",
-            brend: "Laconte",
-            designer: "Johnny Cash",
-            category: "Jackets",
-            season: "Summer",
-            pic: "jacket.jpg"
-        },
-            {
-            id: 0002,
-            title: "Polo",
-            description: "Excellent green polo",
-            price: 50,
-            count: 1,
-            color: "Green",
-            size: "M",
-            brend: "Gusi",
-            designer: "Bob Dylan",
-            category: "Polos",
-            season: "Autumn",
-            pic: "polo.jpg"
-        },
-        {
-            id: 0003,
-            title: "Short",
-            description: "Luxurious black short",
-            price: 100,
-            count: 1,
-            color: "Black",
-            size: "S",
-            brend: "Dolche & Gavvana",
-            category: "Shorts",
-            designer: "James Brown",
-            season: "Summer",
-            pic: "Short.jpg"
-        },
-        {
-            id: 0004,
-            title: "Sweater",
-            description: "Bright white sweater",
-            price: 400,
-            count: 1,
-            color: "White",
-            size: "XL",
-            brend: "Diar",
-            designer: "Elton John",
-            category: "Sweaters",
-            season: "Autumn",
-            pic: "Sweater.jpg"
+    var table = document.createElement('table');
+    table.style.border = "2px solid black";
+    table.style.width = "700px";
+    table.style.height = "700px";
+    table.style.margin = "0 auto";
+    var tr, td, i, j;
+    for (i = 0; i < 8; i++){
+        tr = document.createElement("tr");
+        table.append(tr);
+        for (j = 0; j < 8; j++){
+            td = document.createElement("td");
+            td.style.backgroundColor = (i + j) % 2 ? "black": "white";
+            tr.append(td);
         }
-    ]; 
 
-    console.log(product[1].color);
+    }
 
+    var block = document.getElementById("x");
+    document.body.prepend(table);
 }
