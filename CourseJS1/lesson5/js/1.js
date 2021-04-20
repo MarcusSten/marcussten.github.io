0017219fd53db55c9396b5f6ac473cf900063941
MarcusSten <malmeloon@gmail.com>
2021-04-20 11:30:09 +0300

    edit coursJS1/lesson5

--- CourseJS1/lesson5/js/1.js
+++ CourseJS1/lesson5/js/1.js
@@ -3,9 +3,8 @@ function myScript1(){
     
 	
     var table = document.createElement("table");
-    var shapesWhite = ["&#9817;", "&#9814;", "&#9816;", "&#9815;", "&#9813;", "&#9812;"];
-    var shapesBlack = ["&#9823;", "&#9820;", "&#9822;", "&#9821;", "&#9819;", "&#9818;"];
-    var boardLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+    var shapes = ["&#9823;", "&#9820;", "&#9822;", "&#9821;", "&#9819;", "&#9818;"];
+    var boardLetters = ["<span>A</span>", "B", "C", "D", "E", "F", "G", "H"];
 
     table.style.border = "2px solid black";
     table.style.width = "700px";
@@ -27,26 +26,32 @@ function myScript1(){
     //Расстановка пешек
     for (let i = 1; i <= 8; i++) {
     //Белые пешки
-        table.rows[7].cells[i].innerHTML = shapesWhite[0];
+        table.rows[7].cells[i].innerHTML = shapes[0];
     // Черные пешки
-        table.rows[2].cells[i].innerHTML = shapesBlack[0];
+        table.rows[2].cells[i].innerHTML = shapes[0];
+    }
+
+    //Покраска белых фигур
+    for (let i = 1; i <= 8; i++) {
+        table.rows[7].cells[i].className = "white";
+        table.rows[8].cells[i].className = "white";
     }
 
     //Расстановка остальных фигур
     for (let i = 1,j=8; i <= 3; i++,j--){
         //Белые фигуры
-        table.rows[8].cells[i].innerHTML = shapesWhite[i];
-        table.rows[8].cells[j].innerHTML = shapesWhite[i];
+        table.rows[8].cells[i].innerHTML = shapes[i];
+        table.rows[8].cells[j].innerHTML = shapes[i];
         //Черные фигуры
-        table.rows[1].cells[i].innerHTML = shapesBlack[i];
-        table.rows[1].cells[j].innerHTML = shapesBlack[i];
+        table.rows[1].cells[i].innerHTML = shapes[i];
+        table.rows[1].cells[j].innerHTML = shapes[i];
     }
     //Белые король и ладья
-    table.rows[8].cells[4].innerHTML = shapesWhite[5];
-    table.rows[8].cells[5].innerHTML = shapesWhite[4];
+    table.rows[8].cells[4].innerHTML = shapes[5];
+    table.rows[8].cells[5].innerHTML = shapes[4];
     //Черные король и ладья
-    table.rows[1].cells[4].innerHTML = shapesBlack[4];
-    table.rows[1].cells[5].innerHTML = shapesBlack[5];
+    table.rows[1].cells[4].innerHTML = shapes[4];
+    table.rows[1].cells[5].innerHTML = shapes[5];
 
     //Разметка верт.
     for (let i = 1, j = 8; i <= 8; i++, j--) {
@@ -61,6 +66,7 @@ function myScript1(){
 
     }
 
+    //Классы для стилей CSS разметки
     for (let i = 0; i <= 8; i++) {
         table.rows[0].cells[i].className = "markup-rotate";
         table.rows[i].cells[9].className = "markup-rotate";
