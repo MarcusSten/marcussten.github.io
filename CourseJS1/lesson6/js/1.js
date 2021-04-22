f605060a38c72b505827e8d1f6aedf7daeb8da6b
MarcusSten <malmeloon@gmail.com>
2021-04-22 17:16:36 +0300

    add coursJS1/lesson6

--- CourseJS1/lesson6/js/1.js
+++ CourseJS1/lesson6/js/1.js
@@ -1,7 +1,26 @@
-function myScript1(){
+function init(){
 
+    var images = document.getElementsByTagName("img");
+        for (var i = 0; i < images.length; i++) {
+        images[i].onclick = changeBigPicture;
+        }
+    }
+    function changeBigPicture(eventObj){
+        var appDiv = document.getElementById("big_picture");
+        appDiv.innerHTML = "";
+        var eventElement = eventObj.target;
+        var imageNameParts = eventElement.id.split("_");
+        var src = "img/big/" + imageNameParts[1] + ".jpg";
+        if(src.onerror == true){
+            alert("Картинки не существует");
+        };
+        var imageDomElement = document.createElement("img");
+        imageDomElement.src = src;
+        appDiv.appendChild(imageDomElement);
+        
+    }
+    function alarm(){
+        alert("dfdfd");
+       }
     
-	
-    
-  
-}
+    window.onload = init;
