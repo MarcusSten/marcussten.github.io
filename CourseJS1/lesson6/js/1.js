7ad5dda31298a369884550b223e810c5a459c1ef
MarcusSten <76691268+MarcusSten@users.noreply.github.com>
2021-04-22 22:42:35 +0300

    edit coursJS1/leason6

--- CourseJS1/lesson6/js/1.js
+++ CourseJS1/lesson6/js/1.js
@@ -1,26 +1,27 @@
 function init(){
 
     var images = document.getElementsByTagName("img");
-        for (var i = 0; i < images.length; i++) {
+    for (var i = 0; i < images.length; i++) {
         images[i].onclick = changeBigPicture;
-        }
+        images[i].onerror = errorLoadImg;
     }
-    function changeBigPicture(eventObj){
-        var appDiv = document.getElementById("big_picture");
-        appDiv.innerHTML = "";
-        var eventElement = eventObj.target;
-        var imageNameParts = eventElement.id.split("_");
-        var src = "img/big/" + imageNameParts[1] + ".jpg";
-        if(src.onerror == true){
-            alert("Картинки не существует");
-        };
-        var imageDomElement = document.createElement("img");
-        imageDomElement.src = src;
-        appDiv.appendChild(imageDomElement);
-        
-    }
-    function alarm(){
-        alert("dfdfd");
-       }
+}
+function errorLoadImg(e){
+    e.target.src='https://upload.wikimedia.org/wikipedia/commons/9/9a/%D0%9D%D0%B5%D1%82_%D1%84%D0%BE%D1%82%D0%BE.png'
+     alert("Error load img");
     
-    window.onload = init;
+}
+ function changeBigPicture(eventObj) {
+    var appDiv = document.getElementById("big_picture");
+      appDiv.innerHTML = "";	
+      var eventElement = eventObj.target;
+      var imageNameParts = eventElement.id.split("_");
+      var src = "./img/big/" + imageNameParts[1] + ".jpg";
+      var imageDomElement = document.createElement("img");
+      imageDomElement.src = src;
+    imageDomElement.onerror = errorLoadImg;
+       appDiv.appendChild(imageDomElement);
+  
+  }
+        
+init();
