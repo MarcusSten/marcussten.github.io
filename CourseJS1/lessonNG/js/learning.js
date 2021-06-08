ffe40502cc267c76b793bc7b5ac69aa0b046c9c1
MarcusSten <malmeloon@gmail.com>
2021-06-08 16:49:33 +0300

    edit CourseJS2\lesson5

--- CourseJS1/lessonNG/js/learning.js
+++ CourseJS1/lessonNG/js/learning.js
@@ -877,7 +877,29 @@ function myScript59(){
     
 }
 
+function myScript60(){
+    class Worker {
+        constructor(name, surname, rate, days) {
+            this.name = name,
+            this.surname  = surname,
+            this.rate  = rate,
+            this.days  = days
+        }
 
+        getSalary(rate, days) {
+            return this.rate * this.days;
+        }
 
+    }
 
+    let worker = new Worker('Иван', 'Иванов', 10, 31);
+    let worker2 = new Worker('Маским', 'Петров', 20, 31);
+
+    console.log(worker.name); //выведет 'Иван'
+    console.log(worker.surname); //выведет 'Иванов'
+    console.log(worker.rate); //выведет 10
+    console.log(worker.days); //выведет 31
+    console.log(worker.getSalary()); //выведет 310 - то есть 10*31
+    console.log(worker.getSalary() + worker2.getSalary());
+}
 
