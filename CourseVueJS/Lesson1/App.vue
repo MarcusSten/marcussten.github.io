ec5e7d96bc68b5c480848df81aacef1625bfbc47
MarcusSten <malmeloon@gmail.com>
2021-07-28 16:43:25 +0300

    edit course VueJS/Lesson1 and Brick

---
+++ CourseVueJS/Lesson1/App.vue
@@ -0,0 +1,18 @@
+<template>
+   <div>
+       <input v-model="operand1"/>
+       <input v-model="operand2"/>
+   </div>
+</template>
+ 
+<script>
+ export default {
+   name: 'Calculator',
+   data(){
+       return {
+           operand1: 0,
+           operand2: 0,
+       }
+   }
+ }
+</script>
