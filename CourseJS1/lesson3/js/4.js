74ce52391cccb3ca17c30fc3b8785ee076777d54
MarcusSten <malmeloon@gmail.com>
2021-10-28 11:19:18 +0300

    delete close projects

--- CourseJS1/lesson3/js/4.js
+++
@@ -1,7 +0,0 @@
-function myScript4(){
-    
-    for(var a = 0; a <= 9; console.log(a), a++){
-    };
-
-
-}
--- CourseJS1/lesson3/js/5.js
+++
@@ -1,8 +0,0 @@
-function myScript5(){
-    var egypt = "x";
-    var x;
-    console.log(egypt);
-    for(x = 0; x < 20; x++){
-        console.log(egypt += "x");
-    }
-}
